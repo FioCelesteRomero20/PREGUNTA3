224ed8d1786630d65febefee417683009a148953
PC <PC@DESKTOP-NOE8GP1>
2023-11-03 15:28:23 -0500

    Agregar archivos de proyecto.

---
+++ PREGUNTA3/Program.cs
@@ -0,0 +1,38 @@
+﻿using PREGUNTA3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PREGUNTA3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = InterfazConsola.menuPrincipal();
+            do
+            {
+                switch (opcion)
+                {
+                    case 0:
+                        opcion = InterfazConsola.menuPrincipal();
+                        break;
+                    case 1:
+                        opcion = InterfazConsola.sumar();
+                        break;
+                    case 2:
+                        opcion = InterfazConsola.restar();
+                        break;
+                    case 3:
+                        opcion = InterfazConsola.multiplicar();
+                        break;
+                    case 4:
+                        opcion = InterfazConsola.dividir();
+                        break;
+                }
+            } while (opcion != 5);
+        }
+    }
+}
